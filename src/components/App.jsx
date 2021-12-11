572ec3c99e824ea95638c72795fc11360b2c5bc5
Otavio Ramos <otavio.omak@gmail.com>
2021-12-11 17:28:54 -0300

    challenge 1 done.

---
+++ src/components/App.jsx
@@ -0,0 +1,20 @@
+import React, {useState} from "react";
+
+function App() {
+  let initialTime = new Date().toLocaleTimeString();
+  const [time, setTime] = useState(initialTime);
+
+  function getTime() {
+    let newTime = new Date().toLocaleTimeString();
+    setTime(newTime);
+  }
+
+  return (
+    <div className="container">
+      <h1>{time}</h1>
+      <button onClick={getTime}>Get Time</button>
+    </div>
+  );
+}
+
+export default App;
